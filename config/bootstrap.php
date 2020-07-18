82bebf5f68ad2d7f15bb5f5f0069afefe31a41ce
Ishan Vyas <isvyas@gmail.com>
2020-07-19 00:38:52 +0530

    Update config bootstrap file

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,3 +1,11 @@
 <?php
+use Cake\Core\Configure;
 
 require __DIR__ . '/constants.php';
+
+/**
+ * __PLUGIN__'s default configuration.
+ */
+Configure::write('__PLUGIN__', [
+    // Add your configurations here
+]);
